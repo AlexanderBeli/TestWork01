f516b37c7d2c23d56181b2d5e014c5f58d0c3142
AlexanderBeli <alexandrbelitskiy@gmail.com>
2025-09-11 13:13:52 +0200

    update: for tests

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import PlainTextResponse, RedirectResponse
 
 from database.db import client
 from logger import logging, setup_logging
@@ -31,3 +32,16 @@ async def lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
 app = FastAPI(title="Quotes Scraper API", version="1.0.0", lifespan=lifespan)
 
 app.include_router(router)
+
+
+@app.get("/", include_in_schema=False)
+async def home() -> RedirectResponse:
+    """Redirect home to docs."""
+    return RedirectResponse("/docs")
+
+
+@app.get("/healthcheck", include_in_schema=False)
+@app.head("/healthcheck", include_in_schema=False)
+async def healthcheck() -> PlainTextResponse:
+    """Healthcheck endpoint."""
+    return PlainTextResponse("OK", status_code=status.HTTP_200_OK)
